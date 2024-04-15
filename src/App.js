edb62ffecd9224dfd1de2b024741da669e3f9c44
Muh-Uzair <muhammaduzair1062001@gmail.com>
2024-04-16 04:14:59 +0500

    All complete

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ export default function App() {
             //______________________________________________________________________________//
                     const [height , set_height] = useState("") ;
                     function handle_height_change(event_info_object) {
-                      set_height(event_info_object.target.value) ;
+                      set_height( (event_info_object.target.value)) ;
                     }
 
               
@@ -20,15 +20,48 @@ export default function App() {
                     const [weight , set_weight] = useState("") ;
                     function handle_weight_change(event_info_object) {
                       
-                      console.log(`hello`)
+                      set_weight(event_info_object.target.value)
                     }
 
 
             //______________________________________________________________________________//
                     function handle_calculate_btn_click(event_info_object) {
 
-                      console.log(height , weight);
+                      const bmi = (weight / (height * height)).toFixed(2) ;
 
+                      if (bmi < 18.5) {
+                        set_bmi_msg("Underweight")
+                      } else if (bmi >= 18.5 && bmi <= 24.9) {
+                        set_bmi_msg("Normal weight")
+                      } else if (bmi >= 25 && bmi <= 29.9) {
+                        set_bmi_msg("Overweight")                        
+                      } else {
+                        set_bmi_msg("Obesity")
+                      }
+
+                      set_bmi_output(bmi) ;
+                    }
+
+
+            //______________________________________________________________________________//
+                    function handle_height_btn_click_function(event_info_object , type) {
+
+                      if(type === "positive" )
+                      set_height((height) => (Number(height)+0.01).toFixed(2) ) ;
+                      
+                      if(type === "negative")
+                      set_height((height) => (Number(height)-0.01).toFixed(2) ) ;
+                    }
+
+
+            //______________________________________________________________________________//
+                    function handle_weight_btn_click_function(event_info_object , type) {
+
+                      if(type === "positive" )
+                      set_weight((height) => (Number(weight)+1)) ;
+                      
+                      if(type === "negative")
+                      set_weight((height) => (Number(weight)-1) ) ;
                     }
 
 
@@ -49,22 +82,22 @@ export default function App() {
 
 
       <div className="div_input_height"> 
-        <input type="text" className="input_height" placeholder="Enter height in m" value={height} onChange={(e) => handle_height_change(e)}/>
+        <input type="text" className="input_height" maxLength={4} placeholder="Enter height in m" value={height} onChange={(e) => handle_height_change(e)}/>
         <button className="project_btn button_height_positive">
-          <p className="text_inside text_inside_positive">+</p>
+          <p className="text_inside text_inside_positive" onClick={(e) => handle_height_btn_click_function(e , "positive" )}>+</p>
           </button>
         <button className="project_btn button_height_negative">
-          <p className="text_inside text_inside_negative">-</p>
+          <p className="text_inside text_inside_negative" onClick={(e) => handle_height_btn_click_function(e , "negative" )}>-</p>
         </button>
       </div>
 
       <div className="div_input_weight" > 
-        <input type="text" className="input_weight" placeholder="Enter weight in kg" onChange={(e) => handle_weight_change(e)} />
+        <input type="text" className="input_weight" maxLength={3} placeholder="Enter weight in kg" value={weight} onChange={(e) => handle_weight_change(e)} />
         <button className="project_btn button_weight_positive">
-          <p className="text_inside text_inside_positive">+</p>
+          <p className="text_inside text_inside_positive"  onClick={(e) => handle_weight_btn_click_function(e , "positive" )} >+</p>
         </button>
         <button className="project_btn button_weight_negative">
-          <p className="text_inside text_inside_negative">-</p>
+          <p className="text_inside text_inside_negative"  onClick={(e) => handle_weight_btn_click_function(e , "negative" )} >-</p>
         </button>
       </div>
 
